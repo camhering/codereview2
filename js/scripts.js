db468d449141082ad3630965c434377fcc4c57d7
Cam Hering <cam.hering@gmail.com>
2016-09-25 20:34:21 -0700

    add a whole new project as I jacked the last one up

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var yourSpot = function(beer, outfit, food, weather, travel) {
+  if (beer === "pilsner" && outfit === "jeans and sweater") {
+    return "Canon beach";
+  }
+  else if (beer === "ipa" && outfit === "shorts and flip flops") {
+    return "Kauai";
+  }
+  else if (weather === "sun" && travel === "boat") {
+    return "Vietnam";
+  }
+  else if (beer === "lager" && food === "pizza") {
+    return "Portland, OR"
+  }
+
+  
+
+};
+
+$(document).ready(function() {
+  $("form#vacation").submit(function(event) {
+    var beer = $("select#beer").val();
+    var outfit = $("select#outfit").val();
+    var food = $("select#food").val();
+    var weather = $("select#weather").val();
+    var travel = $("select#travel").val();
+
+
+    var yourspot = yourSpot(beer,outfit,food,weather,travel);
+    $("#yourspot").show();
+    $("#perfect").text(yourspot);
+
+    event.preventDefault();
+
+  });
+});
